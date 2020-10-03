b4e0ee5bf0803b078dbb870c171879a27504e998
Marko Bogicevic <bogicevic.marko1008@gmail.com>
2020-10-03 18:43:10 +0200

    :pencil: Update Basic test

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -1,13 +1,14 @@
 package tests;
 
 import java.io.File;
+import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.poi.hpsf.Date;
-import org.openqa.selenium.OutputType;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,6 +19,7 @@ import org.testng.annotations.BeforeClass;
 
 public abstract class BasicTest {
 
+	protected JavascriptExecutor js;
 	protected WebDriver driver;
 	protected WebDriverWait wait;
 	protected String baseURL = "http://demo.yo-meals.com";
@@ -28,7 +30,8 @@ public abstract class BasicTest {
 	public void beforeClass() {
 		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
 		this.driver = new ChromeDriver();
-
+		this.js = (JavascriptExecutor) driver;
+		this.wait = new WebDriverWait(driver, 30);
 		this.driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		this.driver.manage().window().maximize();
@@ -37,16 +40,16 @@ public abstract class BasicTest {
 
 	@AfterMethod
 	public void afterTest(ITestResult result) throws Exception {
-		String testTime = new SimpleDateFormat("yyyyMMddHHmmss'.txt'").format(new Date());
+		String testTime = new SimpleDateFormat("yyyyMMddHHmmss'.png'").format(new Date());
 		if (result.getStatus() == ITestResult.FAILURE) {
 			TakesScreenshot ts = (TakesScreenshot) driver;
 			File ss = ts.getScreenshotAs(OutputType.FILE);
-			FileUtils.copyDirectory(ss, new File("./screenshots/" + testTime + ".png"));
+			FileUtils.copyFile(ss, new File("screenshots/" + testTime));
 		}
 		this.driver.manage().deleteAllCookies();
 
 	}
-
+	
 	@AfterClass
 	public void afterClass() {
 		this.driver.quit();
